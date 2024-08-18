57e139ffca7e5efb7158e0afd62cac920f28087c
yasmin-bg <yasb367@gmail.com>
2024-08-18 15:32:55 -0300

    Correcoes de padrao

--- bilheteria/src/dao/AdministradorDAO.java
+++ bilheteria/src/dao/AdministradorDAO.java
@@ -3,26 +3,12 @@ package dao;
 import dto.FilmeDTO;
 import dto.FuncionarioDTO;
 
-public class AdministradorDAO implements AdministradorIF {
+public interface AdministradorDAO {
 
-    public void cadastrarFuncionario(String nomeCompleto, int cpf, int senha) {
-   
-    }
-
-    public void editarInformacoesFuncionario(FuncionarioDTO editarInformacao) {
-          
-    }
-
-    public void cadastrarFilme(int id, String titulo, String sinopse, String genero, int duracao, 
-    int classificacaoIndicativa) {
-          
-    }
-
-    public void excluirFilme(int id) {
-          
-    }
-
-    public void cadastrarSalaExibicao(int id, String endereco, FilmeDTO[] filmes) {
-           
-    }
+    public void cadastrarFuncionario(String nomeCompleto, int cpf, int senha);
+    public void editarInformacoesFuncionario(FuncionarioDTO editarInformacao);
+    public void cadastrarFilme(int id, String titulo, String sinopse, String genero, 
+    int duracao, int classificacaoIndicativa);
+    public void excluirFilme(int id);
+    public void cadastrarSalaExibicao(int id, String endereco, FilmeDTO[] filmes);
 }
